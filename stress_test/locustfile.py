22cb3aeacf00c85b386e920f94c3e0206ff040bb
Eze Rosso <erosso@munidigital.com>
2024-05-29 19:54:22 -0300

    add locust test

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,5 +1,17 @@
-from locust import HttpUser, between, task
+from locust import HttpUser, between, task, TaskSet
 
+class UserBehavior(TaskSet):
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def predict(self):
+        # Ajusta la ruta al archivo de imagen que quieres enviar en la solicitud
+        image_path = '/home/ezequiel/Descargas/assignment/assignment/stress_test/dog.jpeg'
+        with open(image_path, 'rb') as image_file:
+            files = {'file': image_file}
+            self.client.post("/predict", files=files)
 
 class APIUser(HttpUser):
     wait_time = between(1, 5)
@@ -7,4 +19,5 @@ class APIUser(HttpUser):
     # Put your stress tests here.
     # See https://docs.locust.io/en/stable/writing-a-locustfile.html for help.
     # TODO
-    raise NotImplementedError
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
